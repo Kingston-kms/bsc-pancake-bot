6bfcf5ebe85202fe3684bae7b75cfb30a8972e6d
Kingston-kms <kingston1853@gmail.com>
2022-01-07 21:58:38 +0300

    pairs

--- src/blockchain/blockchain.constants.ts
+++ src/blockchain/blockchain.constants.ts
@@ -1,9 +1,13 @@
 export const PANCAKE_ROUTER_ADDRESS = '0xD99D1c33F9fC3444f8101754aBC46c52416550D1'
 export const PANCAKE_FACTORY_ADDRESS = '0x6725F303b657a9451d8BA641348b6761A6CC7a17'
 export const BSC_API_ENDPOINT = 'https://data-seed-prebsc-1-s1.binance.org:8545'
+export const DAI_CONTRACT = '0x8a9424745056eb399fd19a0ec26a14316684e274'
+export const BUSD_CONTRACT = '0x78867bbeef44f2326bf8ddd1941a4439382ef2a7'
+export const WBNB_CONTRACT = '0xae13d989dac2f0debff460ac112a837c89baa7cd'
 
 export const routerAbi = [
-    'function getAmountsOut(uint amountIn, address[] memory path) public view returns(uint[] memory amounts)'
+    'function getAmountsOut(uint amountIn, address[] memory path) public view returns (uint[] memory amounts)',
+    'function getAmountsIn(uint amountOut, address[] memory path) public view returns (uint[] memory amounts)'
 ];
 
 export const factoryAbi = [
--- src/blockchain/blockchain.service.ts
+++ src/blockchain/blockchain.service.ts
@@ -1,6 +1,6 @@
 import { Global, Injectable } from '@nestjs/common';
 import { Contract, ethers } from 'ethers';
-import { factoryAbi, PANCAKE_ROUTER_ADDRESS, BSC_API_ENDPOINT, routerAbi, PANCAKE_FACTORY_ADDRESS } from './blockchain.constants';
+import { factoryAbi, PANCAKE_ROUTER_ADDRESS, BSC_API_ENDPOINT, routerAbi, PANCAKE_FACTORY_ADDRESS, BUSD_CONTRACT, WBNB_CONTRACT, DAI_CONTRACT } from './blockchain.constants';
 
 @Injectable()
 export class BlockchainService {
@@ -34,12 +34,26 @@ export class BlockchainService {
     }
 
     async worker() {
-        const count = await this.factory.allPairsLength();
+        const value = ethers.utils.parseUnits('1');
+        const amountsIn1 = await this.router.getAmountsOut(value, [WBNB_CONTRACT, BUSD_CONTRACT]);
+        console.log(ethers.utils.formatUnits(amountsIn1[0]) + ' WBNB');
+        console.log(ethers.utils.formatUnits(amountsIn1[1]) + ' BUSD');
+
+        const amountsIn2 = await this.router.getAmountsOut(value, [WBNB_CONTRACT, DAI_CONTRACT]);
+        console.log(ethers.utils.formatUnits(amountsIn2[0]) + ' WBNB');
+        console.log(ethers.utils.formatUnits(amountsIn2[1]) + ' DAI');
+
+        /*const count = await this.factory.allPairsLength();
         const pairs = [];
         for(let i = 1; i < count; i++) {
             const p = await this.factory.allPairs(i);
             pairs.push(p);
             console.log(p);
         }
+        console.log(pairs);*/
+
+        setTimeout(async () => {
+            await this.worker();    
+        }, 5000);
     }
 }
